9f17b4655943536133781dffe33e659799f35690
thayagapriyan <rdsthayagan@gmail.com>
2026-05-25 02:17:24 -0500

    updated

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,6 +8,11 @@ output "agent_function_arn" {
   value       = aws_lambda_function.agent.arn
 }
 
+output "agent_function_name" {
+  description = "Name of the Strands agent Lambda (used by the smoke test)"
+  value       = aws_lambda_function.agent.function_name
+}
+
 output "bedrock_agent_id" {
   description = "Bedrock AgentCore agent ID"
   value       = aws_bedrockagent_agent.strands_agent.agent_id
